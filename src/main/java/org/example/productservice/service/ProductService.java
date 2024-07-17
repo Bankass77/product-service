bc4f83a79ac62cdb3f7be8f7720d28b5e5ae9737
aguindo <a.guindo@groupeonepoint.com>
2024-07-17 17:04:49 +0200

    add java doc

--- src/main/java/org/example/productservice/service/ProductService.java
+++ src/main/java/org/example/productservice/service/ProductService.java
@@ -17,6 +17,13 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    /**
+     * Crée un nouveau produit en utilisant les détails fournis dans l'objet ProductRequest.
+     *
+     * @param productRequest
+     *         l'objet ProductRequest contenant les informations nécessaires pour créer un nouveau produit. Cet objet inclut des détails tels que le nom, le
+     *         prix et la description du produit.
+     */
     public void createProduct(ProductRequest productRequest) {
 
         Product product = Product.builder().name(productRequest.name()).price(productRequest.price()).description(productRequest.description()).build();
@@ -25,11 +32,23 @@ public class ProductService {
         log.info("Product created: {}", product.getId());
     }
 
+    /**
+     * Récupère tous les produits disponibles dans le dépôt et les renvoie sous forme de liste de ProductResponse.
+     *
+     * @return une liste d'objets ProductResponse représentant tous les produits disponibles.
+     */
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return products.stream().map(this::mapToProductResponse).toList();
     }
 
+    /**
+     * Convertit un objet Product en un objet ProductResponse.
+     *
+     * @param product
+     *         l'objet Product à convertir. Cet objet contient les informations d'un produit telles que l'identifiant, le nom, la description et le prix.
+     * @return un nouvel objet ProductResponse initialisé avec les données provenant de l'objet Product.
+     */
     private ProductResponse mapToProductResponse(Product product) {
         return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
     }
